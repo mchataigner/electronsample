e84122bfb1646f32b40fe3062497d3ab6e22e675
Mathieu Bressolle-Chataigner <mathieu.chataigner@gmail.com>
2020-11-24 19:20:24 +0100

    sample app

--- main.js
+++ main.js
@@ -1,28 +1,36 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserView, BrowserWindow } = require('electron')
 
 function createWindow () {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  })
+	const win = new BrowserWindow({
+		width: 1200,
+		height: 800,
+		minWidth: 800,
+		minHeight: 500,
+		useContentSize: true
+	})
 
-  win.loadFile('index.html')
-  win.webContents.openDevTools()
+	const view = new BrowserView()
+	const margin = 150
+	win.setBrowserView(view)
+	view.setBounds({ x: 0, y: margin, width: 1200, height: 800 - margin })
+	view.webContents.loadURL('https://electronjs.org')
+	
+	win.on('resize', () => {
+		const { width, height } = win.getContentBounds()
+		view.setBounds({x:0, y:margin, width: width, height: height - margin})
+	})
 }
 
 app.whenReady().then(createWindow)
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+	if (process.platform !== 'darwin') {
+		app.quit()
+	}
 })
 
 app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
+	if (BrowserWindow.getAllWindows().length === 0) {
+		createWindow()
+	}
 })
